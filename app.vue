983ae08d4b721e86f241b203639cdce1ddf0ffe2
jalil <jalil@1001.digital>
2024-11-26 22:46:51 +0100

    Add token details

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
 <template>
   <main>
-    <Auction v-if="scope" :id="scope" />
+    <Auction v-if="scope" :id="scope">
+      <template #header-after>
+        <p>
+          ERC-721 on Ethereum
+        </p>
+        <p>
+          Read more on <NuxtLink to="https://www.jack.art/work/latent">Latent</NuxtLink>
+        </p>
+      </template>
+      <template #secondary-details>
+        <aside class="prose">
+          <p>
+            Accompanied by 50x50" framed Silver Gelatin Print (Inverted)
+          </p>
+          <p>
+            Produced at PICTO Laboratory, Bastille, Paris
+          </p>
+          <p>
+            Displayed at Paris Photo 2024
+          </p>
+          <p>
+            Buyer pays shipping
+          </p>
+        </aside>
+      </template>
+    </Auction>
     <NuxtPage v-else />
   </main>
 </template>
