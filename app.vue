5304ee57f4f881d827d0ceb252e71ab8a7baf9f2
jalil <jalil@1001.digital>
2024-11-26 22:31:54 +0100

    Update app

--- app.vue
+++ app.vue
@@ -1,17 +1,24 @@
 <template>
   <main>
-    <Auction id="1" />
+    <Auction v-if="scope" :id="scope" />
+    <NuxtPage v-else />
   </main>
 </template>
 
-<style>
-@import "~/assets/theme.css";
-</style>
+<script setup>
+const config = useRuntimeConfig()
+
+const scope = computed(() => config.public.auctionId)
+</script>
 
 <style scoped>
 main {
   display: grid;
   gap: var(--spacer);
 }
+</style>
+
+<style>
+@import "~/assets/theme.css";
 </style>
 
